4204ff4757c5ea6735748c3e89aed12e2d4b5019
josh <jpollock412@gmail.com>
2023-03-30 18:53:31 -0400

    Repo details

---
+++ README.md
@@ -0,0 +1,34 @@
+# Sources For Plugin Machine 2
+
+
+## Remote Sources
+
+Templates for [Plugin Machine v2](https://pluginmachine.com/wordpress-plugin-generator/)
+
+
+### repo.json
+
+The Github repo that has the actual templates.
+
+```json
+{
+    "owner": "imaginarymachines",
+    "name": "core-style-plugin"
+}
+```
+
+### config.json
+
+
+- @todo document this.
+- See trustedlogin/client/config.json.
+
+### main.php
+
+Optional. A php file to be used to initalize the feature.
+
+- See trustedlogin/client/main.php
+
+### logo.png
+
+Optional. A logo for the template.
---
+++ trustedlogin/client/main.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Configuration for TrustedLogin Client
+ *
+ *
+ * @see https://docs.trustedlogin.com/Client/configuration
+ */
+
+$config = [];
+
+try {
+    // Check class_exists() for sites running PHP 5.2.x
+    if ( class_exists( '\PhpNamespace\TrustedLogin\Client') ) {
+        new \PhpNamespace\TrustedLogin\Client(
+            new \PhpNamespace\TrustedLogin\Client( $config )
+        );
+    }
+} catch ( \Exception $exception ) {
+    error_log( $exception->getMessage() );
+}
+
+/**
+ * Autoloader for the TrustedLogin Client
+ *
+ * @param string $class The fully-qualified class name.
+ * @see https://www.php-fig.org/psr/psr-4/examples/
+ * @return void
+ */
+spl_autoload_register(function ($class) {
+    $prefix = 'PhpNamespace\\TrustedLogin\\';
+    $base_dir = __DIR__ . '/src/';
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    if (file_exists($file)) {
+        require $file;
+    }
+});
